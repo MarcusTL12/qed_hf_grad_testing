e0781e284e52284b191c4d0886bfb0a9d7a69ac8
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-06-16 12:42:12 +0200

    ran overnight

--- runner.jl
+++ runner.jl
@@ -2,6 +2,6 @@
 
 include("md/main.jl")
 
-@time resume_md("md/many_h2o/10h2o_0.1.xyz", 1000, 43)
-# @time resume_md("md/many_h2o/10h2o_free.xyz", 1000, 40)
+@time resume_md("md/many_h2o/10h2o_0.1.xyz", 4000)
+# @time resume_md("md/many_h2o/10h2o_free.xyz", 1000)
 
