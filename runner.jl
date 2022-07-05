cb2ed7e4123018a2f1ac3f7aafd3bca297418638
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-07-05 22:29:59 +0200

    dirac

--- runner.jl
+++ runner.jl
@@ -2,6 +2,6 @@
 
 include("md/main.jl")
 
-@time resume_md("md/many_h2o/10h2o_0.1_prec.xyz", 10000)
+@time resume_md("md/many_h2o/10h2o_0.1_freeze.xyz", 10000)
 # @time resume_md("md/many_h2o/10h2o_free_temp.xyz", 20000; Δt=10.0)
 
