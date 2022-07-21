265c1e8aa2e239403030bf6fa64f86baf9220253
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-07-21 12:10:03 +0200

    Ran while in rome

--- runner.jl
+++ runner.jl
@@ -3,8 +3,8 @@
 # include("numgrad/main.jl")
 include("md/main.jl")
 
-curfile = "md/many_h2o/20h2o_0.1.xyz"
+curfile = "md/many_h2o/50h2o_free.xyz"
 # curfile = "md/many_h2o/20h2o_free.xyz"
 
-@time resume_md(curfile, 10000)
-# @time keep_temp(curfile, 275, 1000, 300)
+# @time resume_md(curfile, 10000)
+@time keep_temp(curfile, 275, 100, 50)
