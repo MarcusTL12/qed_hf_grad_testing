489388468a92b90437e9105354ce82fb6c545726
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-07-25 13:58:56 +0200

    Ran 0.1 on casimir

--- runner.jl
+++ runner.jl
@@ -8,4 +8,4 @@ curfile = "md/many_h2o/30h2o_0.1.xyz"
 # curfile = "md/many_h2o/20h2o_free.xyz"
 
 # @time resume_md(curfile, 10000)
-# @time keep_temp(curfile, 275, 100, 50)
+@time keep_temp(curfile, 275, 100, 50)
